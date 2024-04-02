d2572ca5fa364e1f4045218fb6d7a743895d799b
qinmingjie <1934394876@qq.com>
2024-04-02 13:26:55 +0800

    feat(utils): 添加用户名和密码验证函数

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+export function verifyUserName(rules, value, callback) {
+  const regxUserName = /^[a-zA-Z0-9\u4e00-\u9fa5]{4,16}$/.test(value)
+  if (!value) {
+    callback(new Error('用户名为必填项!'))
+  } else if (!regxUserName) {
+    callback(new Error('用户名为数字,字母或中文不能包含特殊符号且长度在8-16'))
+  } else {
+    callback()
+  }
+}
+
+export function verifyPassWord(rules, value, callback) {
+  const regxPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,16}$/.test(value)
+  if (!value) {
+    callback(new Error('密码为必填项!'))
+  } else if (!regxPassword) {
+    callback(new Error('密码至少包含一个字母,数字,特殊符号(!@#$%^&*),且长度在8-16'))
+  } else {
+    callback()
+  }
+}
