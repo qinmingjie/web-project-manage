220f5abfe734a0d3fe70abad59c75ea61415b6d3
qinmingjie <1934394876@qq.com>
2024-04-02 14:07:55 +0800

    chore(main): 添加相关依赖中间件使用

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
+import 'element-plus/theme-chalk/src/message.scss'
 import App from './App.vue'
+import store from './store/index'
 
 import router from './router/index'
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.mount('#app')
