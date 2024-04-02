23607743d662fc3d0233e633de5af03ea9d72721
qinmingjie <1934394876@qq.com>
2024-04-02 14:06:47 +0800

    chore(api/base.js): 添加基础的请求拦截

--- src/api/base.js
+++ src/api/base.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const instance = axios.create({
   baseURL: 'http://localhost:8881',
@@ -12,8 +13,20 @@ instance.interceptors.request.use(
 )
 
 instance.interceptors.response.use(
-  (res) => res,
-  (err) => Promise.reject(err)
+  (res) => {
+    const { status = '', data = {} } = res || {}
+    const message = data.message || '未知错误,请打开控制台查看!'
+    if (status !== 200) {
+      ElMessage.error({ message })
+      return Promise.reject(res)
+    }
+    return res
+  },
+  (err) => {
+    const { data = {} } = err.response || {}
+    ElMessage.error({ message: data.message || '未知错误,请打开控制台查看!' })
+    return Promise.reject(err)
+  }
 )
 
 export default instance
