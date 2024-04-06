087d7dbd4fa890bc19ea21c97ae487086417f455
qinmingjie <1934394876@qq.com>
2024-04-06 10:53:51 +0800

    feat(jsconfig.json, vite.config): 配置别名和vscode别名路径提示

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -16,7 +17,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/src/'
+      '@': path.join(__dirname, './src')
     }
   }
 })
